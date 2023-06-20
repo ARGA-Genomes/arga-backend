cabb03b1156cf7ad26d7a0a55131f46f7ccbe523
Goran Sterjov <goran.sterjov@csiro.au>
2023-06-20 16:28:57 +1000

    added image upload to admin media page

--- src/http/admin/media.rs
+++ src/http/admin/media.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
+use std::path::Path;
 
-use axum::extract::{State, Query};
-use axum::{Json, Router};
+use axum::body::Bytes;
+use axum::extract::{State, Query, Multipart};
+use axum::{Json, BoxError, Router};
+use axum::http::StatusCode;
 use axum::routing::{get, post};
 
 use diesel::prelude::*;
@@ -10,9 +13,12 @@ use serde::{Deserialize, Serialize};
 use tracing::debug;
 use uuid::Uuid;
 
+use futures::{Stream, TryStreamExt};
+use tokio::{fs::File, io::BufWriter};
+use tokio_util::io::StreamReader;
 
 use crate::http::Context;
-use crate::http::error::InternalError;
+use crate::http::error::{InternalError, Error};
 use crate::database::{schema, Database};
 use crate::database::models::{Media, Name, TaxonPhoto};
 
@@ -136,16 +142,136 @@ async fn upsert_main_media(
 }
 
 
-/// The REST gateway for the admin backend for basic CRUD operations
-pub(crate) fn router() -> Router<Context> {
-    Router::new()
-        .route("/api/admin/media", get(media))
-        .route("/api/admin/media/main", get(main_media))
-        .route("/api/admin/media/main", post(upsert_main_media))
+#[derive(Deserialize, Debug)]
+struct UploadMainImage {
+    file: String,
+    scientific_name: String,
+    source: Option<String>,
+    publisher: Option<String>,
+    license: Option<String>,
+    rights_holder: Option<String>,
+}
+
+async fn upload_main_image(
+    State(db_provider): State<Database>,
+    Json(form): Json<UploadMainImage>,
+) -> Result<(), InternalError>
+{
+    // link the main photo as an attribute against the taxa
+    use schema::{names, taxon_photos};
+    let mut conn = db_provider.pool.get().await?;
+
+    debug!(?form, "setting main image");
+
+    let name: Name = names::table
+        .filter(names::scientific_name.eq(form.scientific_name))
+        .get_result(&mut conn)
+        .await?;
+
+    // move the temporary image file to the image store
+    let filename = format!("arga_admin_{}", form.file);
+    let tmp_path = std::env::var("ADMIN_TMP_UPLOAD_STORAGE").expect("No upload storage specified");
+    let img_path = std::env::var("ADMIN_IMAGE_UPLOAD_STORAGE").expect("No upload storage specified");
+    std::fs::copy(Path::new(&tmp_path).join(&filename), Path::new(&img_path).join(&form.file))?;
+
+    // delete any previous main images
+    diesel::delete(taxon_photos::table)
+        .filter(taxon_photos::name_id.eq(name.id))
+        .execute(&mut conn)
+        .await?;
+
+    // add a taxa photo entry linked to the name
+    let photo = TaxonPhoto {
+        id: Uuid::new_v4(),
+        name_id: name.id,
+        url: form.file,
+        source: form.source,
+        publisher: form.publisher,
+        license: form.license,
+        rights_holder: form.rights_holder,
+    };
+
+    diesel::insert_into(taxon_photos::table)
+        .values(&photo)
+        .execute(&mut conn)
+        .await?;
+
+    debug!(?photo, ?name, "main image set");
+
+    Ok(())
+}
+
+
+#[tracing::instrument]
+async fn accept_image(mut multipart: Multipart) -> Result<String, Error> {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        if let Some(file_name) = field.file_name() {
+            let uuid = uuid::Uuid::new_v4();
+            let tmp_name = format!("arga_admin_{}", uuid.to_string());
+            debug!(?uuid, file_name, tmp_name, "Receiving file");
+            stream_to_file(&tmp_name, field).await.unwrap();
+
+            return Ok(uuid.to_string());
+        }
+    }
+
+    Ok("".into())
+}
+
+async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
+where
+    S: Stream<Item = Result<Bytes, E>>,
+    E: Into<BoxError>,
+{
+    if !valid_path(path) {
+        return Err((StatusCode::BAD_REQUEST, "Invalid path".to_owned()));
+    }
+
+    async {
+        // get an async reader from the body stream
+        let body = stream.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err));
+        let reader = StreamReader::new(body);
+        futures::pin_mut!(reader);
+
+        // create the file on the server
+        let tmp_path = std::env::var("ADMIN_TMP_UPLOAD_STORAGE").expect("No upload storage specified");
+        let path = std::path::Path::new(&tmp_path).join(path);
+        let mut file = BufWriter::new(File::create(path).await?);
+
+        tokio::io::copy(&mut reader, &mut file).await?;
+
+        Ok::<_, std::io::Error>(())
+    }
+    .await
+    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
 }
 
 
 fn parse_int_param(params: &HashMap<String, String>, name: &str, default: i64) -> i64 {
     let val = params.get(name).map(|val| val.parse::<i64>().unwrap_or(default)).unwrap_or(default);
     if val <= 0 { 1 } else { val }
+}
+
+fn valid_path(path: &str) -> bool {
+    let path = std::path::Path::new(path);
+    let mut components = path.components().peekable();
+
+    if let Some(first) = components.peek() {
+        if !matches!(first, std::path::Component::Normal(_)) {
+            return false;
+        }
+    }
+
+    components.count() == 1
+}
+
+
+/// The REST gateway for the admin backend for basic CRUD operations
+pub(crate) fn router() -> Router<Context> {
+    Router::new()
+        .route("/api/admin/media", get(media))
+        .route("/api/admin/media/main", get(main_media))
+        .route("/api/admin/media/main", post(upsert_main_media))
+        .route("/api/admin/media/upload", post(accept_image))
+        .route("/api/admin/media/upload_main_image", post(upload_main_image))
 }
