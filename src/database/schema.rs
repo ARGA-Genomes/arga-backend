f844e6caa5297dd02e7a8b54c59f6101cddc4861
Goran Sterjov <goran.sterjov@csiro.au>
2023-07-05 13:07:43 +1000

    removed spatial_ref_sys from diesel schema since its not needed

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -350,16 +350,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    spatial_ref_sys (srid) {
-        srid -> Int4,
-        auth_name -> Nullable<Varchar>,
-        auth_srid -> Nullable<Int4>,
-        srtext -> Nullable<Varchar>,
-        proj4text -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
     specimens (id) {
         id -> Uuid,
         list_id -> Uuid,
