5a9f2020f9a7d88ece2b4f38b0c2cbfaf294fc76
Goran Sterjov <goran.sterjov@csiro.au>
2023-09-22 20:06:38 +1000

    updated views to reflect record_id changes

--- core/migrations/2023-09-18-131726_create_markers_view/up.sql
+++ core/migrations/2023-09-18-131726_create_markers_view/up.sql
@@ -5,7 +5,7 @@ SELECT
     sequences.name_id,
     sequences.dna_extract_id,
     datasets.name AS dataset_name,
-    sequences.accession,
+    sequences.record_id,
     sequencing_events.sequenced_by,
     sequencing_events.material_sample_id,
     sequencing_events.target_gene
--- core/migrations/2023-09-19-071526_create_specimen_stats/up.sql
+++ core/migrations/2023-09-19-071526_create_specimen_stats/up.sql
@@ -1,9 +1,9 @@
 CREATE MATERIALIZED VIEW specimen_stats AS
 SELECT DISTINCT
     specimens.id,
-    SUM(CASE WHEN sequences.accession IS NOT NULL THEN 1 ELSE 0 END) over (partition BY specimens.accession ORDER BY specimens.accession DESC) AS sequences,
-    SUM(CASE WHEN annotation_events.representation IN ('Full', 'Partial') THEN 1 ELSE 0 END) over (partition BY specimens.accession ORDER BY specimens.accession DESC) AS whole_genomes,
-    SUM(CASE WHEN sequencing_events.target_gene IS NOT NULL THEN 1 ELSE 0 END) over (partition BY specimens.accession ORDER BY specimens.accession DESC) AS markers
+    SUM(CASE WHEN sequences.record_id IS NOT NULL THEN 1 ELSE 0 END) over (partition BY specimens.record_id ORDER BY specimens.record_id DESC) AS sequences,
+    SUM(CASE WHEN annotation_events.representation IN ('Full', 'Partial') THEN 1 ELSE 0 END) over (partition BY specimens.record_id ORDER BY specimens.record_id DESC) AS whole_genomes,
+    SUM(CASE WHEN sequencing_events.target_gene IS NOT NULL THEN 1 ELSE 0 END) over (partition BY specimens.record_id ORDER BY specimens.record_id DESC) AS markers
 FROM specimens
 LEFT JOIN subsamples ON subsamples.specimen_id = specimens.id
 LEFT JOIN dna_extracts ON dna_extracts.subsample_id = subsamples.id
