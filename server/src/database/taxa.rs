51d2f672408bf900748c1ea4e88a192a1dc07adc
Goran Sterjov <secure@goran.email>
2024-06-07 18:18:47 +1000

    taxon history endpoint now pulls taxa history from taxa connected to the same name. allows cross system history

--- server/src/database/taxa.rs
+++ server/src/database/taxa.rs
@@ -317,21 +317,32 @@ impl TaxaProvider {
     }
 
     pub async fn history(&self, taxon_id: &Uuid) -> Result<Vec<HistoryItem>, Error> {
-        use schema::{datasets, name_publications as publications, taxa, taxon_history as history};
+        use schema::{datasets, name_publications as publications, taxa, taxon_history as history, taxon_names};
         let mut conn = self.pool.get().await?;
 
+        let name_ids = taxon_names::table
+            .select(taxon_names::name_id)
+            .filter(taxon_names::taxon_id.eq(taxon_id))
+            .into_boxed();
+
+        let taxon_ids = taxon_names::table
+            .select(taxon_names::taxon_id)
+            .filter(taxon_names::name_id.eq_any(name_ids))
+            .load::<Uuid>(&mut conn)
+            .await?;
+
         let synonym_original_descriptions = history::table
-            .filter(history::acted_on.eq(taxon_id))
+            .filter(history::acted_on.eq_any(taxon_ids.clone()))
             .select(history::taxon_id)
             .into_boxed();
 
         let items = history::table
             .inner_join(datasets::table)
-            .inner_join(taxa::table.on(taxa::id.eq(history::taxon_id)))
+            .inner_join(taxa::table.on(taxa::id.eq(history::acted_on)))
             .inner_join(nomenclatural_acts::table)
             .left_join(publications::table)
             .filter(history::taxon_id.eq(taxon_id))
-            .or_filter(history::acted_on.eq(taxon_id))
+            .or_filter(history::acted_on.eq_any(taxon_ids))
             .or_filter(history::taxon_id.eq_any(synonym_original_descriptions))
             .select(HistoryItem::as_select())
             .order((publications::published_year.asc(), taxa::scientific_name.asc()))
