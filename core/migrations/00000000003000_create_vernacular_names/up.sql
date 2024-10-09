d5d12af33db50b90a824100eee9d3ad2f2ebb596
Goran Sterjov <secure@goran.email>
2024-10-09 18:41:38 +1100

    fix existing table error for vernacular names migration

--- core/migrations/00000000003000_create_vernacular_names/up.sql
+++ core/migrations/00000000003000_create_vernacular_names/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE vernacular_names (
+CREATE TABLE IF NOT EXISTS vernacular_names (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     dataset_id uuid REFERENCES datasets NOT NULL,
     name_id uuid REFERENCES names NOT NULL,
@@ -7,4 +7,4 @@ CREATE TABLE vernacular_names (
     source_url varchar
 );
 
-CREATE UNIQUE INDEX vernacular_names_unique_name ON vernacular_names (dataset_id, name_id, vernacular_name);
+CREATE UNIQUE INDEX IF NOT EXISTS vernacular_names_unique_name ON vernacular_names (dataset_id, name_id, vernacular_name);
