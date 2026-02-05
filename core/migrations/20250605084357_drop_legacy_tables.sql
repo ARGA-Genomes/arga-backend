53e160bebb205af11a77e5f1fd50070dd04c5683
Goran Sterjov <secure@goran.email>
2026-02-05 15:20:17 +1100

    fix drop legacy migration

--- core/migrations/20250605084357_drop_legacy_tables.sql
+++ core/migrations/20250605084357_drop_legacy_tables.sql
@@ -1,24 +1,24 @@
 -- Drop index "nomenclatrural_acts_name" from table: "nomenclatural_acts"
-DROP INDEX "public"."nomenclatrural_acts_name";
+DROP INDEX IF EXISTS "public"."nomenclatrural_acts_name";
 -- Drop index "nomenclatural_acts_unique_entity" from table: "nomenclatural_acts"
-DROP INDEX "public"."nomenclatural_acts_unique_entity";
+DROP INDEX IF EXISTS "public"."nomenclatural_acts_unique_entity";
 -- Create index "nomenclatural_acts_entity" to table: "nomenclatural_acts"
 CREATE UNIQUE INDEX "nomenclatural_acts_entity" ON "public"."nomenclatural_acts" ("entity_id");
 -- Drop "assembly_stats" table
-DROP TABLE "public"."assembly_stats";
+DROP TABLE IF EXISTS "public"."assembly_stats";
 -- Drop "biosamples" table
-DROP TABLE "public"."biosamples";
+DROP TABLE IF EXISTS "public"."biosamples";
 -- Drop "ecology" table
-DROP TABLE "public"."ecology";
+DROP TABLE IF EXISTS "public"."ecology";
 -- Drop "indigenous_knowledge" table
-DROP TABLE "public"."indigenous_knowledge";
+DROP TABLE IF EXISTS "public"."indigenous_knowledge";
 -- Drop "organisms" table
-DROP TABLE "public"."organisms";
+DROP TABLE IF EXISTS "public"."organisms";
 -- Drop "trace_files" table
-DROP TABLE "public"."trace_files";
+DROP TABLE IF EXISTS "public"."trace_files";
 -- Drop "assemblies" table
-DROP TABLE "public"."assemblies";
+DROP TABLE IF EXISTS "public"."assemblies";
 -- Drop "taxon_history" table
-DROP TABLE "public"."taxon_history";
+DROP TABLE IF EXISTS "public"."taxon_history";
 -- Drop "name_publications" table
-DROP TABLE "public"."name_publications";
+DROP TABLE IF EXISTS "public"."name_publications";
